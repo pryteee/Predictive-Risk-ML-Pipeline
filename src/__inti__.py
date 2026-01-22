d3cbc4624e318508a3f449ef38eb9367b4eadbf5
Pritirekha <pritirekhapanda06@gmail.com>
2026-01-22 21:53:21 +0530

    Remove all files from GitHub, keep locally

--- src/__inti__.py
+++
@@ -1,7 +0,0 @@
-"""
-Student Dropout Prediction Package
-"""
-
-__version__ = "1.0.0"
-__author__ = "Student Dropout Prediction Team"
-__description__ = "Machine Learning pipeline for predicting student dropout risk"
